58d32c039cf0ba5efc20fa980d8822b82683bf4b
BlazarQSO <BlazarQSO@gmail.com>
2019-09-23 20:00:03 +0300

    expression-calculator/feat: add solution

--- src/index.js
+++ src/index.js
@@ -4,7 +4,96 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    const oper = [")", "(", "+", "-", "*", "/"];
+    let reversExpr = [];
+
+    expr = expr.trim();
+
+    let arrExpr = expr.split(" ");
+    while (arrExpr.includes("")) {
+        let index = arrExpr.indexOf("");
+        arrExpr.splice(index, 1);
+    }
+
+
+    let stack = [];
+    for (let i = 0; i < arrExpr.length; i++) {
+        if (!oper.includes(arrExpr[i])) {
+            reversExpr.push(arrExpr[i]);
+
+        } else if (arrExpr[i] == "(") {
+            stack.push(arrExpr[i]);
+
+        } else if (arrExpr[i] == ")") {
+            while (true) {
+                let pop = "";
+
+                if (stack.length > 0) {
+                    pop = stack.pop();
+                } else {
+                    return Number("error");
+                }
+
+                if (pop != "(") {
+                    reversExpr.push(pop);
+                } else {
+                    break;
+                }
+            }
+
+        } else if (oper.includes(arrExpr[i])) {
+            while (true) {
+                if (oper.indexOf(stack[stack.length - 1]) > 1
+                    && oper.indexOf(arrExpr[i]) > 1
+                    && oper.indexOf(arrExpr[i]) < 4
+                    || (oper.indexOf(stack[stack.length - 1]) > 3
+                    && oper.indexOf(arrExpr[i]) > 3)) {
+                    reversExpr.push(stack.pop());
+                } else {
+                    stack.push(arrExpr[i]);
+                    break;
+                }
+            }
+        }
+    }
+
+    while (stack.length > 0) {
+        reversExpr.push(stack.pop());
+    }
+
+    let stackCount = [];
+    for (let i = 0; i < reversExpr.length; i++) {
+        if (!oper.includes(reversExpr[i])) {
+            stackCount.push(reversExpr[i]);
+        } else if (oper.includes(reversExpr[i])) {
+            let b = stackCount.pop();
+            let a = stackCount.pop();
+            switch (reversExpr[i]) {
+                case "+": {
+                    stackCount.push(Number(a) + Number(b));
+                    break;
+                }
+                case "-": {
+                    stackCount.push(Number(a) - Number(b));
+                    break;
+                }
+                case "*": {
+                    stackCount.push(Number(a) * Number(b));
+                    break;
+                }
+                case "/": {
+                    if (Number(b) != 0) {
+                        stackCount.push(Number(a) / Number(b));
+                    } else {
+                        stackCount.unshift(Infinity);
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    return stackCount.pop();
 }
 
 module.exports = {
